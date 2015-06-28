9a7e0e3db189fd88dc7793fa72ed237681f127e8
lordnovas <joelrainey@gmail.com>
2015-06-27 19:00:35 -0700

    Deleted Test file i/o methods and Begin Adding file i/o methods to DataBaseAdapter.
    Will handle current file(session aka csv) as a single ref within the DataBaseAdapter Class. The current design of the Db class has each method that deals with the csv file opening/closing the outputStream rather than ref a single ouputStream  i'm not sure if it matters but I can swap if it does.

--- app/src/main/java/com/demo/joel/meditationpro/DataBaseAdapter.java
+++ app/src/main/java/com/demo/joel/meditationpro/DataBaseAdapter.java
@@ -8,16 +8,51 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class DataBaseAdapter
 {
     DataBaseHelper helper;
+    File currCSVFile;
 
     public DataBaseAdapter(Context context)
     {
         helper = new DataBaseHelper(context);
     }
 
+
+    /**
+     * Under Construction:
+     * Adding DataPoints from Array of Integers up next
+     */
+
+    public File createCSV(String filename) {
+        return currCSVFile = new File(helper.context.getFilesDir(), filename);
+    }
+
+    public void setCurrCSVFile(File currCSVFile) {
+        this.currCSVFile = currCSVFile;
+    }
+
+    public void addSingleDataPoint(int time, int device_val) {
+        FileOutputStream outputStream;
+        try {
+            outputStream = new FileOutputStream(currCSVFile);
+            outputStream.write(time);
+            outputStream.write(":".getBytes());
+            outputStream.write(device_val);
+            outputStream.write(",".getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     //Insert a Single Meditation Value
     public long insert_MedVal(int medVal)
@@ -42,37 +77,27 @@ public class DataBaseAdapter
 
 
 
-
-
-
     static class DataBaseHelper extends SQLiteOpenHelper
     {
-        private Context context;
-
         //Name of the Database
         private static final String DATABASE_NAME = "MeditationPro.db";
-
         //Database Version Number
         private static final  int DATABASE_VERSION=1;
-
         //Table Name(s)
         private static final String TABLE_TEST ="Test_Table";
-
         //Row ID
         private static final String UID = "_id";
-
         //Column(s)
         private static final String MEDITATION_VALUE = "meditationValue";
-
         //Create Statement for TABLE_TEST
         private static final String CREATE_TABLE = "CREATE TABLE "
                 + TABLE_TEST + " " +
                 "("+ UID +" INTEGER PRIMARY KEY AUTOINCREMENT, "
                  + MEDITATION_VALUE + " INTEGER);";
-
         //Drop Statement for Table_TEST
         private static final String DROP_TABLE_TEST
                 = "DROP TABLE IF EXISTS "+ TABLE_TEST;
+        private Context context;
 
 
         public DataBaseHelper(Context context)
